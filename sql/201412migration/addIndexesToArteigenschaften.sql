e435f3322b19ed6eeb3ea98a9603145a599a63ec
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-12-15 15:53:50 +0100

    admindb: Bearbeiten in Tabellen funktioniert. Arteigenschaften haben Primary key

--- sql/201412migration/addIndexesToArteigenschaften.sql
+++ sql/201412migration/addIndexesToArteigenschaften.sql
@@ -1,2 +1,2 @@
-ALTER TABLE alexande_beob.ArtenDb_Arteigenschaften ADD UNIQUE INDEX `guid` (`GUID`);
 ALTER TABLE alexande_beob.ArtenDb_Arteigenschaften ADD INDEX `taxonomieid` (TaxonomieId);
+ALTER TABLE alexande_beob.ArtenDb_Arteigenschaften ADD PRIMARY KEY(`GUID`);
