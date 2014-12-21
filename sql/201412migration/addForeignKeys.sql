08e29508c2ea2874cc9e41880de1ed48931a1588
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-12-21 22:21:24 +0100

    Strukturumbau vorbereitet

--- sql/201412migration/addForeignKeys.sql
+++ sql/201412migration/addForeignKeys.sql
@@ -1,3 +1,17 @@
+ALTER TABLE pop DROP FOREIGN KEY tblpop_popid;
+ALTER TABLE ber DROP FOREIGN KEY tblber_apartid;
+ALTER TABLE assozart DROP FOREIGN KEY tblassozarten_apartid;
+ALTER TABLE idealbiotop DROP FOREIGN KEY tblidealbiotop_apartid;
+ALTER TABLE popber DROP FOREIGN KEY tblpopber_apartid;
+ALTER TABLE popmassnber DROP FOREIGN KEY tblpopmassnber_popid;
+ALTER TABLE tpopber DROP FOREIGN KEY tbltpopber_tpopid;
+ALTER TABLE tpopkontr DROP FOREIGN KEY tbltpopkontr_tpopid;
+ALTER TABLE tpop DROP FOREIGN KEY tbltpop_popid;
+ALTER TABLE beobzuordnung DROP FOREIGN KEY tblbeobzuordnung_tpopid;
+ALTER TABLE tpopkontrzaehl DROP FOREIGN KEY tpopkontrzaehl_tpopkontrid;
+ALTER TABLE tpopmassn DROP FOREIGN KEY tbltpopmassn_tpopid;
+ALTER TABLE tpopmassnber DROP FOREIGN KEY tbltpopmassnber_tpopid;
+ALTER TABLE zielber DROP FOREIGN KEY tblzielber_zielid;
 ALTER TABLE `pop` ADD CONSTRAINT `tblpop_popid` FOREIGN KEY (`ApArtId`) REFERENCES `apflora`.`ap`(`ApArtId`) ON DELETE SET NULL ON UPDATE CASCADE;
 ALTER TABLE `ber` ADD CONSTRAINT `tblber_apartid` FOREIGN KEY (`ApArtId`) REFERENCES `apflora`.`ap`(`ApArtId`) ON DELETE NO ACTION ON UPDATE CASCADE;
 ALTER TABLE `assozart` ADD CONSTRAINT `tblassozarten_apartid` FOREIGN KEY (`AaApArtId`) REFERENCES `apflora`.`ap`(`ApArtId`) ON DELETE SET NULL ON UPDATE CASCADE;
@@ -15,39 +29,3 @@ ALTER TABLE `zielber` ADD  CONSTRAINT `tblzielber_zielid` FOREIGN KEY (`ZielId`)
 ALTER TABLE `apber` ADD CONSTRAINT `tbljber_apartid` FOREIGN KEY (`ApArtId`) REFERENCES `apflora`.`ap`(`ApArtId`) ON DELETE NO ACTION ON UPDATE CASCADE;
 ALTER TABLE `erfkrit` ADD CONSTRAINT `tblerfkrit_apartid` FOREIGN KEY (`ApArtId`) REFERENCES `apflora`.`ap`(`ApArtId`) ON DELETE NO ACTION ON UPDATE CASCADE;
 ALTER TABLE `ziel` ADD CONSTRAINT `tblziel_apartid` FOREIGN KEY (`ApArtId`) REFERENCES `apflora`.`ap`(`ApArtId`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
-
-
-
-select TABLE_NAME,COLUMN_NAME,CONSTRAINT_NAME,REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME from INFORMATION_SCHEMA.KEY_COLUMN_USAGE where REFERENCED_COLUMN_NAME IS NOT NULL order by REFERENCED_COLUMN_NAME;
-
-show fields from ziel where Field = 'ApArtId';
-show fields from ap where Field = 'ApArtId';
-
-ALTER TABLE pop DROP FOREIGN KEY tblpop_popid;
-ALTER TABLE ber DROP FOREIGN KEY tblber_apartid;
-ALTER TABLE assozart DROP FOREIGN KEY tblassozarten_apartid;
-ALTER TABLE idealbiotop DROP FOREIGN KEY tblidealbiotop_apartid;
-ALTER TABLE popber DROP FOREIGN KEY tblpopber_apartid;
-ALTER TABLE popmassnber DROP FOREIGN KEY tblpopmassnber_popid;
-ALTER TABLE tpopber DROP FOREIGN KEY tbltpopber_tpopid;
-ALTER TABLE tpopkontr DROP FOREIGN KEY tbltpopkontr_tpopid;
-ALTER TABLE tpop DROP FOREIGN KEY tbltpop_popid;
-ALTER TABLE beobzuordnung DROP FOREIGN KEY tblbeobzuordnung_tpopid;
-ALTER TABLE tpopkontrzaehl DROP FOREIGN KEY tpopkontrzaehl_tpopkontrid;
-ALTER TABLE tpopmassn DROP FOREIGN KEY tbltpopmassn_tpopid;
-ALTER TABLE tpopmassnber DROP FOREIGN KEY tbltpopmassnber_tpopid;
-ALTER TABLE zielber DROP FOREIGN KEY tblzielber_zielid;
-
-select * from information_schema.columns where table_name = 'apber' and column_name = 'ApArtId';
-select * from information_schema.columns where table_name = 'ap' and column_name = 'ApArtId';
-
-
-SELECT ENGINE, TABLE_COLLATION
-FROM information_schema.TABLES
-WHERE
-  TABLE_SCHEMA='apflora';
-
-mysqlcheck -u root --password=cA3c6FAYPk -A --auto-repair --all-databases
-
-show create table tpop;
--- sql/201412migration/addIndexesToArteigenschaften.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE apflora_beob.adb_eigenschaften ADD INDEX `taxonomieid` (TaxonomieId);
-ALTER TABLE apflora_beob.adb_eigenschaften ADD PRIMARY KEY(`GUID`);
