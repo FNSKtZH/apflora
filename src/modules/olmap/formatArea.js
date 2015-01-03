a7b965a8c69c20d1f67cbbe7674e372b854525b4
Alexander Gabriel <alex.barbalex@gmail.com>
2015-01-03 01:14:56 +0100

    olmap Flächenmessung: m2 > ha > km2

--- src/modules/olmap/formatArea.js
+++ src/modules/olmap/formatArea.js
@@ -14,8 +14,10 @@ module.exports = function (polygon) {
     var area = polygon.getArea(),
         output;
 
-    if (area > 10000) {
+    if (area > 1000000) {
         output = (Math.round(area / 1000000 * 100) / 100) + ' km<sup>2</sup>';
+    } else if (area > 10000) {
+        output = (Math.round(area / 10000 * 100) / 100) + ' ha';
     } else {
         output = (Math.round(area * 100) / 100) + ' m<sup>2</sup>';
     }
