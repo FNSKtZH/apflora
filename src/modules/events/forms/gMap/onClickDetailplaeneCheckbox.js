f0289f1ad1ed14323fa8be9ff3e9e08ed5523deb
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-01-09 21:48:24 +0100

    Google Karten: Detailpläne wieder sichtbar

---
+++ src/modules/events/forms/gMap/onClickDetailplaeneCheckbox.js
@@ -0,0 +1,12 @@
+/*jslint node: true, browser: true, nomen: true, todo: true */
+'use strict';
+
+var $ = require('jquery');
+
+module.exports = function () {
+    if ($(this).is(':checked')) {
+        window.apf.googleKarteDetailplaene.setMap(window.apf.gMap.map);
+    } else {
+        window.apf.googleKarteDetailplaene.setMap(null);
+    }
+};
