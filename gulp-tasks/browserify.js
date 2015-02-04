a3b0a48ea9e50a6d34eccef6a201db090baaca3e
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-02-04 16:25:40 +0100

    watchify entfernt

--- gulp-tasks/browserify.js
+++ gulp-tasks/browserify.js
@@ -1,11 +1,3 @@
-/* browserify task
- ---------------
- Bundle javascripty things with browserify!
-
- If the watch task is running, this uses watchify instead
- of browserify for faster bundling using caching.
- */
-
 var browserify   = require('browserify');
 var gulp         = require('gulp');
 var source       = require('vinyl-source-stream');
