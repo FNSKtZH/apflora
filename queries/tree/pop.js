4eee466f654ae0a8bd0959f50689d4dc7e6c4fd9
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-02-11 23:34:32 +0100

    Fehler beim Erstellen neuer Programme behoben

--- queries/tree/pop.js
+++ queries/tree/pop.js
@@ -70,152 +70,163 @@ module.exports = function (request, reply) {
             }
         }
     ], function (err, result) {
-        var popIds    = result[0],
-            tpopIds   = result[1],
-            popListe  = result[2],
-            tpopListe = result[3];
+        var popIds        = result[0],
+            tpopIds       = result[1],
+            popListe      = result[2],
+            tpopListe     = result[3],
+            popOrdnerNode = {};
 
-        // jetzt parallel alle übrigen Daten aus dem pop-baum
-        async.parallel({
-            tpopMassnListe: function (callback) {
-                connection.query(
-                    'SELECT TPopMassnId, TPopId, TPopMassnJahr, TPopMassnDatum, MassnTypTxt FROM tpopmassn LEFT JOIN tpopmassn_typ_werte ON TPopMassnTyp = MassnTypCode where TPopId in (' + tpopIds.join() + ') ORDER BY TPopMassnJahr, TPopMassnDatum, MassnTypTxt',
-                    function (err, data) {
-                        callback(err, data);
-                    }
-                );
-            },
-            tpopMassnBerListe: function (callback) {
-                connection.query(
-                    'SELECT TPopMassnBerId, TPopId, TPopMassnBerJahr, BeurteilTxt FROM tpopmassnber LEFT JOIN tpopmassn_erfbeurt_werte ON TPopMassnBerErfolgsbeurteilung = BeurteilId where TPopId in (' + tpopIds.join() + ') ORDER BY TPopMassnBerJahr, BeurteilTxt',
-                    function (err, data) {
-                        callback(err, data);
-                    }
-                );
-            },
-            tpopFeldkontrListe: function (callback) {
-                connection.query(
-                    'SELECT TPopKontrId, TPopId, TPopKontrJahr, TPopKontrTyp FROM tpopkontr where (TPopId in (' + tpopIds.join() + ')) AND (TPopKontrTyp<>"Freiwilligen-Erfolgskontrolle" OR TPopKontrTyp IS NULL) ORDER BY TPopKontrJahr, TPopKontrTyp',
-                    function (err, data) {
-                        callback(err, data);
-                    }
-                );
-            },
-            tpopFreiwkontrListe : function (callback) {
-                connection.query(
-                    'SELECT TPopKontrId, TPopId, TPopKontrJahr, TPopKontrTyp FROM tpopkontr where (TPopId in (' + tpopIds.join() + ')) AND (TPopKontrTyp="Freiwilligen-Erfolgskontrolle") ORDER BY TPopKontrJahr, TPopKontrTyp',
-                    function (err, data) {
-                        callback(err, data);
-                    }
-                );
-            },
-            tpopBerListe: function (callback) {
-                connection.query(
-                    'SELECT TPopBerId, TPopId, TPopBerJahr, EntwicklungTxt, EntwicklungOrd FROM tpopber LEFT JOIN tpop_entwicklung_werte ON TPopBerEntwicklung = EntwicklungCode where TPopId in (' + tpopIds.join() + ') ORDER BY TPopBerJahr, EntwicklungOrd',
-                    function (err, data) {
-                        callback(err, data);
-                    }
-                );
-            },
-            tpopBeobZugeordnetListe: function (callback) {
-                connection.query(
-                    'SELECT apflora.beobzuordnung.NO_NOTE, apflora.beobzuordnung.TPopId, apflora.beobzuordnung.beobNichtZuordnen, apflora.beobzuordnung.BeobBemerkungen, apflora.beobzuordnung.BeobMutWann, apflora.beobzuordnung.BeobMutWer, apflora_beob.beob_bereitgestellt.Datum, apflora_beob.beob_bereitgestellt.Autor, "evab" AS beobtyp FROM apflora.beobzuordnung INNER JOIN apflora_beob.beob_bereitgestellt ON apflora.beobzuordnung.NO_NOTE = apflora_beob.beob_bereitgestellt.NO_NOTE_PROJET WHERE apflora.beobzuordnung.TPopId in (' + tpopIds.join() + ') AND (apflora.beobzuordnung.beobNichtZuordnen=0 OR apflora.beobzuordnung.beobNichtZuordnen IS NULL) UNION SELECT apflora.beobzuordnung.NO_NOTE, apflora.beobzuordnung.TPopId, apflora.beobzuordnung.beobNichtZuordnen, apflora.beobzuordnung.BeobBemerkungen, apflora.beobzuordnung.BeobMutWann, apflora.beobzuordnung.BeobMutWer, apflora_beob.beob_bereitgestellt.Datum, apflora_beob.beob_bereitgestellt.Autor, "infospezies" AS beobtyp FROM apflora.beobzuordnung INNER JOIN apflora_beob.beob_bereitgestellt ON apflora.beobzuordnung.NO_NOTE = apflora_beob.beob_bereitgestellt.NO_NOTE WHERE apflora.beobzuordnung.TPopId in (' + tpopIds.join() + ') AND (apflora.beobzuordnung.beobNichtZuordnen=0 OR apflora.beobzuordnung.beobNichtZuordnen IS NULL) ORDER BY Datum',
-                    function (err, data) {
-                        callback(err, data);
-                    }
-                );
-            },
-            popBerListe: function (callback) {
-                connection.query(
-                    'SELECT PopBerId, PopId, PopBerJahr, EntwicklungTxt, EntwicklungOrd FROM popber LEFT JOIN pop_entwicklung_werte ON PopBerEntwicklung = EntwicklungId where PopId in (' + popIds.join() + ') ORDER BY PopBerJahr, EntwicklungOrd',
-                    function (err, data) {
-                        callback(err, data);
-                    }
-                );
-            },
-            popMassnBerListe: function (callback) {
-                connection.query(
-                    'SELECT PopMassnBerId, PopId, PopMassnBerJahr, BeurteilTxt, BeurteilOrd FROM popmassnber LEFT JOIN tpopmassn_erfbeurt_werte ON PopMassnBerErfolgsbeurteilung = BeurteilId where PopId in (' + popIds.join() + ') ORDER BY PopMassnBerJahr, BeurteilOrd',
-                    function (err, data) {
-                        callback(err, data);
-                    }
-                );
-            }
-        }, function (err, results) {
-            var popBerListe      = results.popBerListe      || [],
-                popMassnBerListe = results.popMassnBerListe || [],
-                popOrdnerNode    = {},
-                popOrdnerNodeChildren,
-                popNrMax;
+        if (tpopIds.length > 0) {
+            // jetzt parallel alle übrigen Daten aus dem pop-baum
+            async.parallel({
+                tpopMassnListe: function (callback) {
+                    connection.query(
+                        'SELECT TPopMassnId, TPopId, TPopMassnJahr, TPopMassnDatum, MassnTypTxt FROM tpopmassn LEFT JOIN tpopmassn_typ_werte ON TPopMassnTyp = MassnTypCode where TPopId in (' + tpopIds.join() + ') ORDER BY TPopMassnJahr, TPopMassnDatum, MassnTypTxt',
+                        function (err, data) {
+                            callback(err, data);
+                        }
+                    );
+                },
+                tpopMassnBerListe: function (callback) {
+                    connection.query(
+                        'SELECT TPopMassnBerId, TPopId, TPopMassnBerJahr, BeurteilTxt FROM tpopmassnber LEFT JOIN tpopmassn_erfbeurt_werte ON TPopMassnBerErfolgsbeurteilung = BeurteilId where TPopId in (' + tpopIds.join() + ') ORDER BY TPopMassnBerJahr, BeurteilTxt',
+                        function (err, data) {
+                            callback(err, data);
+                        }
+                    );
+                },
+                tpopFeldkontrListe: function (callback) {
+                    connection.query(
+                        'SELECT TPopKontrId, TPopId, TPopKontrJahr, TPopKontrTyp FROM tpopkontr where (TPopId in (' + tpopIds.join() + ')) AND (TPopKontrTyp<>"Freiwilligen-Erfolgskontrolle" OR TPopKontrTyp IS NULL) ORDER BY TPopKontrJahr, TPopKontrTyp',
+                        function (err, data) {
+                            callback(err, data);
+                        }
+                    );
+                },
+                tpopFreiwkontrListe : function (callback) {
+                    connection.query(
+                        'SELECT TPopKontrId, TPopId, TPopKontrJahr, TPopKontrTyp FROM tpopkontr where (TPopId in (' + tpopIds.join() + ')) AND (TPopKontrTyp="Freiwilligen-Erfolgskontrolle") ORDER BY TPopKontrJahr, TPopKontrTyp',
+                        function (err, data) {
+                            callback(err, data);
+                        }
+                    );
+                },
+                tpopBerListe: function (callback) {
+                    connection.query(
+                        'SELECT TPopBerId, TPopId, TPopBerJahr, EntwicklungTxt, EntwicklungOrd FROM tpopber LEFT JOIN tpop_entwicklung_werte ON TPopBerEntwicklung = EntwicklungCode where TPopId in (' + tpopIds.join() + ') ORDER BY TPopBerJahr, EntwicklungOrd',
+                        function (err, data) {
+                            callback(err, data);
+                        }
+                    );
+                },
+                tpopBeobZugeordnetListe: function (callback) {
+                    connection.query(
+                        'SELECT apflora.beobzuordnung.NO_NOTE, apflora.beobzuordnung.TPopId, apflora.beobzuordnung.beobNichtZuordnen, apflora.beobzuordnung.BeobBemerkungen, apflora.beobzuordnung.BeobMutWann, apflora.beobzuordnung.BeobMutWer, apflora_beob.beob_bereitgestellt.Datum, apflora_beob.beob_bereitgestellt.Autor, "evab" AS beobtyp FROM apflora.beobzuordnung INNER JOIN apflora_beob.beob_bereitgestellt ON apflora.beobzuordnung.NO_NOTE = apflora_beob.beob_bereitgestellt.NO_NOTE_PROJET WHERE apflora.beobzuordnung.TPopId in (' + tpopIds.join() + ') AND (apflora.beobzuordnung.beobNichtZuordnen=0 OR apflora.beobzuordnung.beobNichtZuordnen IS NULL) UNION SELECT apflora.beobzuordnung.NO_NOTE, apflora.beobzuordnung.TPopId, apflora.beobzuordnung.beobNichtZuordnen, apflora.beobzuordnung.BeobBemerkungen, apflora.beobzuordnung.BeobMutWann, apflora.beobzuordnung.BeobMutWer, apflora_beob.beob_bereitgestellt.Datum, apflora_beob.beob_bereitgestellt.Autor, "infospezies" AS beobtyp FROM apflora.beobzuordnung INNER JOIN apflora_beob.beob_bereitgestellt ON apflora.beobzuordnung.NO_NOTE = apflora_beob.beob_bereitgestellt.NO_NOTE WHERE apflora.beobzuordnung.TPopId in (' + tpopIds.join() + ') AND (apflora.beobzuordnung.beobNichtZuordnen=0 OR apflora.beobzuordnung.beobNichtZuordnen IS NULL) ORDER BY Datum',
+                        function (err, data) {
+                            callback(err, data);
+                        }
+                    );
+                },
+                popBerListe: function (callback) {
+                    connection.query(
+                        'SELECT PopBerId, PopId, PopBerJahr, EntwicklungTxt, EntwicklungOrd FROM popber LEFT JOIN pop_entwicklung_werte ON PopBerEntwicklung = EntwicklungId where PopId in (' + popIds.join() + ') ORDER BY PopBerJahr, EntwicklungOrd',
+                        function (err, data) {
+                            callback(err, data);
+                        }
+                    );
+                },
+                popMassnBerListe: function (callback) {
+                    connection.query(
+                        'SELECT PopMassnBerId, PopId, PopMassnBerJahr, BeurteilTxt, BeurteilOrd FROM popmassnber LEFT JOIN tpopmassn_erfbeurt_werte ON PopMassnBerErfolgsbeurteilung = BeurteilId where PopId in (' + popIds.join() + ') ORDER BY PopMassnBerJahr, BeurteilOrd',
+                        function (err, data) {
+                            callback(err, data);
+                        }
+                    );
+                }
+            }, function (err, results) {
+                var popBerListe      = results.popBerListe      || [],
+                    popMassnBerListe = results.popMassnBerListe || [],
+                    popOrdnerNodeChildren,
+                    popNrMax;
 
-            if (err) { return reply(err); }
+                if (err) { return reply(err); }
 
+                // node für apOrdnerPop aufbauen
+                popOrdnerNode.data     = 'Populationen (' + popListe.length + ')';
+                popOrdnerNode.attr     = {
+                    id: 'apOrdnerPop' + apId,
+                    typ: 'apOrdnerPop'
+                };
+                popOrdnerNodeChildren  = [];
+                popOrdnerNode.children = popOrdnerNodeChildren;
+
+                // PopNr: Je nach Anzahl Stellen der maximalen PopNr bei denjenigen mit weniger Nullen
+                // Nullen voranstellen, damit sie im tree auch als String richtig sortiert werden
+                popNrMax = _.max(popListe, function (pop) {
+                    return pop.PopNr;
+                }).PopNr;
+
+                _.each(popListe, function (pop) {
+                    var popNode         = {},
+                        popNodeChildren = [],
+                        popMassnberOrdnerNode,
+                        popBerOrdnerNode,
+                        popTpopOrdnerNode,
+                        data,
+                        popSort;
+
+                    pop.PopNr = ergaenzePopNrUmFuehrendeNullen(popNrMax, pop.PopNr);
+
+                    // nodes für pop aufbauen
+                    if (pop.PopName && pop.PopNr) {
+                        data    = pop.PopNr + ": " + pop.PopName;
+                        popSort = pop.PopNr;
+                    } else if (pop.PopNr) {
+                        data    = pop.PopNr + ": (kein Name)";
+                        popSort = pop.PopNr;
+                    } else if (pop.PopName) {
+                        data    = "(keine Nr): " + pop.PopName;
+                        // pop ohne Nummern zuunterst sortieren
+                        popSort = 1000;
+                    } else {
+                        data    = "(keine Nr, kein Name)";
+                        popSort = 1000;
+                    }
+
+                    popNode.data = data;
+                    popNode.attr = {
+                        id:   pop.PopId,
+                        typ:  'pop',
+                        sort: popSort
+                    };
+                    // popNode.children ist ein Array, der enthält: popOrdnerTpop, popOrdnerPopber, popOrdnerMassnber
+                    popNode.children = popNodeChildren;
+
+                    popOrdnerNodeChildren.push(popNode);
+
+                    // tpopOrdner aufbauen
+                    popTpopOrdnerNode = erstelleTpopOrdner(results, tpopListe, pop);
+                    popNodeChildren.push(popTpopOrdnerNode);
+
+                    // PopberOrdner aufbauen
+                    popBerOrdnerNode = erstellePopBerOrdner(popBerListe, pop);
+                    popNodeChildren.push(popBerOrdnerNode);
+
+                    // MassnberOrdner aufbauen
+                    popMassnberOrdnerNode = erstellePopMassnBerOrdner(popMassnBerListe, pop);
+                    popNodeChildren.push(popMassnberOrdnerNode);
+                });
+                reply(null, popOrdnerNode);
+            });
+        } else {
             // node für apOrdnerPop aufbauen
-            popOrdnerNode.data     = 'Populationen (' + popListe.length + ')';
+            popOrdnerNode.data     = 'Populationen (0)';
             popOrdnerNode.attr     = {
                 id: 'apOrdnerPop' + apId,
                 typ: 'apOrdnerPop'
             };
-            popOrdnerNodeChildren  = [];
-            popOrdnerNode.children = popOrdnerNodeChildren;
-
-            // PopNr: Je nach Anzahl Stellen der maximalen PopNr bei denjenigen mit weniger Nullen
-            // Nullen voranstellen, damit sie im tree auch als String richtig sortiert werden
-            popNrMax = _.max(popListe, function (pop) {
-                return pop.PopNr;
-            }).PopNr;
-
-            _.each(popListe, function (pop) {
-                var popNode         = {},
-                    popNodeChildren = [],
-                    popMassnberOrdnerNode,
-                    popBerOrdnerNode,
-                    popTpopOrdnerNode,
-                    data,
-                    popSort;
-
-                pop.PopNr = ergaenzePopNrUmFuehrendeNullen(popNrMax, pop.PopNr);
-
-                // nodes für pop aufbauen
-                if (pop.PopName && pop.PopNr) {
-                    data    = pop.PopNr + ": " + pop.PopName;
-                    popSort = pop.PopNr;
-                } else if (pop.PopNr) {
-                    data    = pop.PopNr + ": (kein Name)";
-                    popSort = pop.PopNr;
-                } else if (pop.PopName) {
-                    data    = "(keine Nr): " + pop.PopName;
-                    // pop ohne Nummern zuunterst sortieren
-                    popSort = 1000;
-                } else {
-                    data    = "(keine Nr, kein Name)";
-                    popSort = 1000;
-                }
-
-                popNode.data = data;
-                popNode.attr = {
-                    id:   pop.PopId,
-                    typ:  'pop',
-                    sort: popSort
-                };
-                // popNode.children ist ein Array, der enthält: popOrdnerTpop, popOrdnerPopber, popOrdnerMassnber
-                popNode.children = popNodeChildren;
-
-                popOrdnerNodeChildren.push(popNode);
-
-                // tpopOrdner aufbauen
-                popTpopOrdnerNode = erstelleTpopOrdner(results, tpopListe, pop);
-                popNodeChildren.push(popTpopOrdnerNode);
-
-                // PopberOrdner aufbauen
-                popBerOrdnerNode = erstellePopBerOrdner(popBerListe, pop);
-                popNodeChildren.push(popBerOrdnerNode);
-
-                // MassnberOrdner aufbauen
-                popMassnberOrdnerNode = erstellePopMassnBerOrdner(popMassnBerListe, pop);
-                popNodeChildren.push(popMassnberOrdnerNode);
-            });
+            popOrdnerNode.children = [];
             reply(null, popOrdnerNode);
-        });
+        }
     });
 };
