f64b46828b20afe35eed7e76d5b25629989267ba
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-02-13 17:11:33 +0100

    prod build verbessert

--- gulp-tasks/prod_copy.js
+++ gulp-tasks/prod_copy.js
@@ -17,13 +17,8 @@ return gulp.task(
     function () {
         gulp.start([
             'prod_copy_geojson_to_dist',
-            'prod_copy_queries_to_dist',
-            'prod_copy_src_lib_to_dist',
-            'prod_copy_src_modules_to_dist',
-            'prod_copy_src_templates_to_dist',
             'prod_copy_src_themes_to_dist',
             'prod_copy_src_to_dist',
-            'prod_copy_shp_to_dist',
             'prod_copy_img_to_dist',
             'prod_copy_kml_to_dist',
             'prod_copy_etc_to_dist',
--- gulp-tasks/prod_copy_src_modules_to_dist.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('prod_copy_src_modules_to_dist', function () {
-    return gulp.src('src/modules/**/*')
-        .pipe(gulp.dest('dist/src/modules'));
-});
