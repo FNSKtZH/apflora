99d977d864b4b14c68e9d9d21c4b84977f34ea34
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-02-25 18:43:39 +0100

    build verbessert

--- gulp-tasks/prod_copy_kml_to_dist.js
+++ gulp-tasks/prod_copy_kml_to_dist.js
@@ -1,3 +1,6 @@
+/*jslint node: true, browser: true, nomen: true, todo: true */
+'use strict';
+
 var gulp = require('gulp');
 
 gulp.task('prod_copy_kml_to_dist', function () {
--- gulp-tasks/prod_sftp.js
+++ gulp-tasks/prod_sftp.js
@@ -5,11 +5,15 @@
 /*jslint node: true, browser: true, nomen: true, todo: true */
 'use strict';
 
-var gulp       = require('gulp'),
-    requireDir = require('require-dir');
+var gulp        = require('gulp'),
+    requireDir  = require('require-dir'),
+    runSequence = require('run-sequence');
 
 requireDir('../gulp-tasks', {recurse: true});
 
-return gulp.task('prod_sftp', ['prod_sftp_sftp'], function () {
-    gulp.start('prod_clean_dist');
+return gulp.task('prod_sftp', function () {
+    runSequence(
+        'prod_sftp_sftp',
+        'prod_clean_dist'
+    );
 });
