e185704eeba26028a7b93c3c4269adadf959573c
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-03-10 21:53:02 +0100

    triggers korrigiert, weil sie updates verhinderten

--- sql/triggers2.sql
+++ sql/triggers2.sql
@@ -8,7 +8,7 @@ CREATE TRIGGER check_tpopmassnber_year_per_tpop_unique_update BEFORE UPDATE ON `
     FOR EACH ROW 
         BEGIN
         DECLARE msg varchar(255);
-        IF (NEW.TPopMassnBerJahr IN (SELECT TPopMassnBerJahr FROM tpopmassnber WHERE TPopId = NEW.TPopId))
+        IF (NEW.TPopMassnBerJahr IS NOT NULL AND NEW.TPopMassnBerJahr IN (SELECT TPopMassnBerJahr FROM tpopmassnber WHERE TPopId = NEW.TPopId AND TPopMassnBerId <> NEW.TPopMassnBerId))
         THEN
             SET msg = 'Constraint check_tpopmassnber_year_per_tpop_unique violated: only one tpopmassnber per tpop and year allowed';
             SIGNAL sqlstate '45000' SET message_text = msg;
@@ -23,7 +23,7 @@ CREATE TRIGGER check_tpopmassnber_year_per_tpop_unique_insert BEFORE INSERT ON `
     FOR EACH ROW 
         BEGIN
         DECLARE msg varchar(255);
-        IF (NEW.TPopMassnBerJahr IN (SELECT TPopMassnBerJahr FROM tpopmassnber WHERE TPopId = NEW.TPopId))
+        IF (NEW.TPopMassnBerJahr IS NOT NULL AND NEW.TPopMassnBerJahr IN (SELECT TPopMassnBerJahr FROM tpopmassnber WHERE TPopId = NEW.TPopId AND TPopMassnBerId <> NEW.TPopMassnBerId))
         THEN
             SET msg = 'Constraint check_tpopmassnber_year_per_tpop_unique violated: only one tpopmassnber per tpop and year allowed';
             SIGNAL sqlstate '45000' SET message_text = msg;
@@ -38,7 +38,7 @@ CREATE TRIGGER check_popmassnber_year_per_pop_unique_update BEFORE UPDATE ON `po
     FOR EACH ROW 
         BEGIN
         DECLARE msg varchar(255);
-        IF (NEW.PopMassnBerJahr IN (SELECT PopMassnBerJahr FROM popmassnber WHERE PopId = NEW.PopId))
+        IF (NEW.PopMassnBerJahr IS NOT NULL AND NEW.PopMassnBerJahr IN (SELECT PopMassnBerJahr FROM popmassnber WHERE PopId = NEW.PopId AND PopMassnBerId <> NEW.PopMassnBerId))
         THEN
             SET msg = 'Constraint check_popmassnber_year_per_pop_unique violated: only one popmassnber per pop and year allowed';
             SIGNAL sqlstate '45000' SET message_text = msg;
@@ -53,7 +53,7 @@ CREATE TRIGGER check_popmassnber_year_per_pop_unique_insert BEFORE INSERT ON `po
     FOR EACH ROW 
         BEGIN
         DECLARE msg varchar(255);
-        IF (NEW.PopMassnBerJahr IN (SELECT PopMassnBerJahr FROM popmassnber WHERE PopId = NEW.PopId))
+        IF (NEW.PopMassnBerJahr IS NOT NULL AND NEW.PopMassnBerJahr IN (SELECT PopMassnBerJahr FROM popmassnber WHERE PopId = NEW.PopId AND PopMassnBerId <> NEW.PopMassnBerId))
         THEN
             SET msg = 'Constraint check_popmassnber_year_per_pop_unique violated: only one popmassnber per pop and year allowed';
             SIGNAL sqlstate '45000' SET message_text = msg;
@@ -68,7 +68,7 @@ CREATE TRIGGER check_popber_year_per_pop_unique_update BEFORE UPDATE ON `popber`
     FOR EACH ROW 
         BEGIN
         DECLARE msg varchar(255);
-        IF (NEW.PopBerJahr IN (SELECT PopBerJahr FROM popber WHERE PopId = NEW.PopId))
+        IF (NEW.PopBerJahr IS NOT NULL AND NEW.PopBerJahr IN (SELECT PopBerJahr FROM popber WHERE PopId = NEW.PopId AND PopBerId <> NEW.PopBerId))
         THEN
             SET msg = 'Constraint check_popber_year_per_pop_unique violated: only one popber per pop and year allowed';
             SIGNAL sqlstate '45000' SET message_text = msg;
@@ -83,7 +83,7 @@ CREATE TRIGGER check_popber_year_per_pop_unique_insert BEFORE INSERT ON `popber`
     FOR EACH ROW 
         BEGIN
         DECLARE msg varchar(255);
-        IF (NEW.PopBerJahr IN (SELECT PopBerJahr FROM popber WHERE PopId = NEW.PopId))
+        IF (NEW.PopBerJahr IS NOT NULL AND NEW.PopBerJahr IN (SELECT PopBerJahr FROM popber WHERE PopId = NEW.PopId AND PopBerId <> NEW.PopBerId))
         THEN
             SET msg = 'Constraint check_popber_year_per_pop_unique violated: only one popber per pop and year allowed';
             SIGNAL sqlstate '45000' SET message_text = msg;
@@ -98,7 +98,7 @@ CREATE TRIGGER check_tpopber_year_per_pop_unique_update BEFORE UPDATE ON `tpopbe
     FOR EACH ROW 
         BEGIN
         DECLARE msg varchar(255);
-        IF (NEW.TPopBerJahr IN (SELECT TPopBerJahr FROM tpopber WHERE TPopId = NEW.TPopId))
+        IF (NEW.TPopBerJahr IS NOT NULL AND NEW.TPopBerJahr IN (SELECT TPopBerJahr FROM tpopber WHERE TPopId = NEW.TPopId AND tPopBerId <> NEW.TPopBerId))
         THEN
             SET msg = 'Constraint check_tpopber_year_per_pop_unique violated: only one tpopber per pop and year allowed';
             SIGNAL sqlstate '45000' SET message_text = msg;
@@ -113,7 +113,7 @@ CREATE TRIGGER check_tpopber_year_per_pop_unique_insert BEFORE INSERT ON `tpopbe
     FOR EACH ROW 
         BEGIN
         DECLARE msg varchar(255);
-        IF (NEW.TPopBerJahr IN (SELECT TPopBerJahr FROM tpopber WHERE TPopId = NEW.TPopId))
+        IF (NEW.TPopBerJahr IS NOT NULL AND NEW.TPopBerJahr IN (SELECT TPopBerJahr FROM tpopber WHERE TPopId = NEW.TPopId AND tPopBerId <> NEW.TPopBerId))
         THEN
             SET msg = 'Constraint check_tpopber_year_per_pop_unique violated: only one tpopber per pop and year allowed';
             SIGNAL sqlstate '45000' SET message_text = msg;
