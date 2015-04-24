1a2a68545c91c69e3def14ba1c41210df2d43e77
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-04-24 09:56:52 +0200

    QK: tpopber ohne Entwicklung

--- src/modules/initiiereQualitaetskontrollen.js
+++ src/modules/initiiereQualitaetskontrollen.js
@@ -118,6 +118,7 @@ module.exports = function (apId) {
     addDataFromViewToQsList(qsList, 'v_qk_freiwkontrzaehlung_ohneanzahl');
     // TPop-Bericht ohne Jahr/Entwicklung
     addDataFromViewToQsList(qsList, 'v_qk_tpopber_ohnejahr');
+    addDataFromViewToQsList(qsList, 'v_qk_tpopber_ohneentwicklung');
     // Pop-Bericht/Pop-Massn.-Bericht ohne Jahr/Entwicklung
     // Ziel ohne Jahr/Zieltyp/Ziel
     // Ziel-Bericht ohne Jahr/Entwicklung
