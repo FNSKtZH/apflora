25808cd21521498590e0ca8d68a3033c3e9c32d9
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-05-04 12:28:23 +0200

    QK: pop erloschen, tpop ansaatversuch

--- src/modules/kontrolliereProgramm.js
+++ src/modules/kontrolliereProgramm.js
@@ -102,6 +102,8 @@ module.exports = function (berichtjahr) {
     createUrlToAddDataFromViewToQsList(qsList, 'v_qk_pop_statusansaatversuchmittpopursprerloschen');
     // Population: Status ist "erloschen" (ursprünglich oder angesiedelt), es gibt aber eine Teilpopulation mit Status "aktuell" (ursprünglich oder angesiedelt)
     createUrlToAddDataFromViewToQsList(qsList, 'v_qk_pop_statuserloschenmittpopaktuell');
+    // Population: Status ist "erloschen" (ursprünglich oder angesiedelt), es gibt aber eine Teilpopulation mit Status "angesiedelt, Ansaatversuch":
+    createUrlToAddDataFromViewToQsList(qsList, 'v_qk_pop_statuserloschenmittpopansaatversuch');
     // tpop ohne Nr/Flurname/Status/bekannt seit/Koordinaten
     createUrlToAddDataFromViewToQsList(qsList, 'v_qk_tpop_ohnenr');
     createUrlToAddDataFromViewToQsList(qsList, 'v_qk_tpop_ohneflurname');
