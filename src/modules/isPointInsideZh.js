1c44464332161e97ee46aadc00faac968cb0967b
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-05-05 23:11:44 +0200

    Code optimiert

--- src/modules/isPointInsideZh.js
+++ src/modules/isPointInsideZh.js
@@ -14,8 +14,6 @@ module.exports = function (x, y) {
         isInsideZh = false;
 
     // convert koordinates to wgs84
-    window.proj4.defs("EPSG:21781", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs");
-
     koordWgs84 = ol.proj.transform(koordLv03, 'EPSG:21781', 'EPSG:4326');
 
     // convert coordinates into a geoJson point
@@ -34,5 +32,6 @@ module.exports = function (x, y) {
 
     // let turf check if the point is in zh
     isInsideZh = turf.inside(koordPt, zhGeojson);
+
     return isInsideZh;
 };
