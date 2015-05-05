470d21fa4f17c35fc42399c6763933cd7c8793b3
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-05-05 23:11:44 +0200

    Code optimiert

--- src/apflora.js
+++ src/apflora.js
@@ -63,6 +63,9 @@ window.apf.initiiereApp = function () {
     };
 
     preventMousescrollEvent($);
+
+    // ol soll Koordinaten von/in lv03 transformieren können
+    window.proj4.defs("EPSG:21781", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs");
 };
 
 // gleich ein mal ausführen
