ceefe357532c8af48ec39ddff874c5c42c1acd48
Alexander Gabriel <alex@gabriel-software.ch>
2015-05-06 12:00:38 +0200

    QK: kontrolliereRelevanzAusserkantonalerTPop begonnen

--- src/modules/kontrolliereRelevanzAusserkantonalerTpop.js
+++ src/modules/kontrolliereRelevanzAusserkantonalerTpop.js
@@ -1,11 +1,34 @@
 /*jslint node: true, browser: true, nomen: true, todo: true */
 'use strict';
 
-var $ = require('jquery'),
-    isPointInsideZh = require('./isPointInsideZh');
+var $                    = require('jquery'),
+    _                    = require('underscore'),
+    isPointInsidePolygon = require('./isPointInsidePolygon'),
+    zhGeojson            = require('../../geojson/ktZh.json'),
+    melde                = require('./melde');
 
 module.exports = function () {
-    // get list of tpop's of this localStorage.apId
+    var qsList = window.apf.qsList;
 
-    // 
+    // get list of tpop's of this localStorage.apId
+    $.ajax({
+        type: 'get',
+        url: 'api/v1/tpopKoordFuerProgramm/apId=' + localStorage.apId
+    }).done(function (data) {
+        // Rückgabewert null wird offenbar auch als success gewertet, gibt weiter unten Fehler, also Ausführung verhindern
+        if (data && data[0]) {
+            data = data[0];
+            _.each(data, function (tpop) {
+                if (tpop.TPopApBerichtRelevant === 1 && !isPointInsidePolygon(zhGeojson, tpop.TPopXKoord, tpop.TPopYKoord)) {
+                    // diese tpop liegt ausserhalb des Kantons, daher sollte sie nicht relevant sein
+                    // von qsList benötigte Daten anfügen
+
+                    // an qsList anfügen
+
+                }
+            });
+        }
+    }).fail(function () {
+        melde('Fehler: Keine Daten für Teilpopulationen erhalten');
+    });
 };
