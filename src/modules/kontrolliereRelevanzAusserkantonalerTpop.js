5bc22ebe932e5904b82ce6a3e2fe61c4afc01120
Alexander Gabriel <alex@gabriel-software.ch>
2015-05-06 11:22:33 +0200

    ktZhGeoJSON wird effizienter geholt

---
+++ src/modules/kontrolliereRelevanzAusserkantonalerTpop.js
@@ -0,0 +1,11 @@
+/*jslint node: true, browser: true, nomen: true, todo: true */
+'use strict';
+
+var $ = require('jquery'),
+    isPointInsideZh = require('./isPointInsideZh');
+
+module.exports = function () {
+    // get list of tpop's of this localStorage.apId
+
+    // 
+};
