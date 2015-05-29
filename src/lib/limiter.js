b8af6b67e088d840e0e5deb24398a5bcf73ab1ad
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-05-29 20:16:00 +0200

    evaded limiter error

--- src/lib/limiter.js
+++ src/lib/limiter.js
@@ -10,12 +10,15 @@ module.exports = function ($) {
   $.fn.extend({
     limiter: function (limit, elem) {
       function setCount (src, elem) {
-        var chars = src.value.length
-        if (chars > limit) {
-          src.value = src.value.substr(0, limit)
-          chars = limit
+        // sometimes src was undifined ???
+        if (src && src.value) {
+          var chars = src.value.length
+          if (chars > limit) {
+            src.value = src.value.substr(0, limit)
+            chars = limit
+          }
+          elem.html(limit - chars)
         }
-        elem.html(limit - chars)
       }
       $(this).on('keyup focus', function () {
         setCount(this, elem)
