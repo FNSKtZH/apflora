bb8c2f89a31d2aeb155c4916f3eba74fdb00b8f0
Alexander Gabriel <alex.barbalex@gmail.com>
2015-11-14 19:51:48 +0100

    optimize code

--- src/modules/isPointInsidePolygon.js
+++ src/modules/isPointInsidePolygon.js
@@ -19,9 +19,6 @@ module.exports = function (polygon, x, y) {
   // convert koordinates to wgs84
   koordWgs84 = ol.proj.transform(koordLv03, 'EPSG:21781', 'EPSG:4326')
 
-  // kann es sein, dass die Kordinaten verkehrt sind?
-  // koordWgs84 = [koordWgs84[1], koordWgs84[0]]
-
   // convert coordinates into a geoJson point
   koordPt = {
     'type': 'Feature',
