db005be5de3e3cb915e375377fd4c38fe7e6933c
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-19 16:38:20 +0100

    updated hapi

--- server.js
+++ server.js
@@ -4,83 +4,86 @@
 
 'use strict'
 
-var // wird nur in Entwicklung genutzt
+/*var // wird nur in Entwicklung genutzt
   serverOptionsDevelopment = {
     debug: {
       log: ['error'],
       request: ['error']
     }
-  },
-  Hapi = require('hapi'),
+  },*/
+var Hapi = require('hapi'),
+  Inert = require('inert'),
   server = new Hapi.Server()
 
-server.connection({
-  host: '0.0.0.0',
-  port: 4000
-})
+server.register(Inert, function () {
+  server.connection({
+    host: '0.0.0.0',
+    port: 4000
+  })
 
-server.start(function (err) {
-  if (err) {
-    throw err
-  }
-  console.log('Server running at:', server.info.uri)
-})
-
-server.route({
-  method: 'GET',
-  path: '/{path*}',
-  handler: function (request, reply) {
-    reply.file('index.html')
-  }
-})
-
-server.route({
-  method: 'GET',
-  path: '/src/{param*}',
-  handler: {
-    directory: {
-      path: 'src'
+  server.start(function (err) {
+    if (err) {
+      throw err
     }
-  }
-})
+    console.log('Server running at:', server.info.uri)
+  })
 
-server.route({
-  method: 'GET',
-  path: '/style/images/{param*}',
-  handler: {
-    directory: {
-      path: 'style/images'
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: function (request, reply) {
+      reply.file('index.html')
     }
-  }
-})
+  })
 
-/* Versuch, funktioniert nicht*/
-server.route({
-  method: 'GET',
-  path: '/etc/{param*}',
-  handler: {
-    directory: {
-      path: 'etc'
+  server.route({
+    method: 'GET',
+    path: '/src/{param*}',
+    handler: {
+      directory: {
+        path: 'src'
+      }
     }
-  }
-})
+  })
 
-server.route({
-  method: 'GET',
-  path: '/style/{param*}',
-  handler: {
-    directory: {
-      path: 'style'
+  server.route({
+    method: 'GET',
+    path: '/style/images/{param*}',
+    handler: {
+      directory: {
+        path: 'style/images'
+      }
     }
-  }
-})
+  })
 
-server.route({
-  method: 'GET',
-  path: '/img/{param*}',
-  handler: {
-    directory: {
-      path: 'img'
+  /* Versuch, funktioniert nicht*/
+  server.route({
+    method: 'GET',
+    path: '/etc/{param*}',
+    handler: {
+      directory: {
+        path: 'etc'
+      }
     }
-  }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/style/{param*}',
+    handler: {
+      directory: {
+        path: 'style'
+      }
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/img/{param*}',
+    handler: {
+      directory: {
+        path: 'img'
+      }
+    }
+  })
 })
