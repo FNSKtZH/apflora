402ea50413fa6aac6179d8953de0c75e8577346c
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-20 14:50:55 +0100

    prevent problem with gulp-handlebars

--- gulp-tasks/templates.js
+++ gulp-tasks/templates.js
@@ -1,12 +1,13 @@
 'use strict'
 
 var gulp = require('gulp'),
-  handlebars = require('gulp-handlebars'),
+  gulpHandlebars = require('gulp-handlebars'),
+  handlebars = require('handlebars'),
   defineModule = require('gulp-define-module')
 
 gulp.task('templates', function () {
   return gulp.src('src/templatesDev/**/*')
-    .pipe(handlebars())
+    .pipe(gulpHandlebars({ handlebars: handlebars }))
     .pipe(defineModule('node'))
     .pipe(gulp.dest('./src/templates'))
 })
