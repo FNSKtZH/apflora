0bf7db357663c4f3f72e5e38e39949d47d9ef422
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-30 15:53:49 +0100

    delay saving options and checkboxes

--- src/modules/events/forms/onChangeSpeichern.js
+++ src/modules/events/forms/onChangeSpeichern.js
@@ -3,5 +3,18 @@
 var speichern = require('../../speichern')
 
 module.exports = function () {
-  speichern(this)
+  /**
+   * Problem: Sometimes two speichern calls come at nearly the same momen
+   * for instance, when a number input was changed, followed dicrectly
+   * by choosing an option/checkbox
+   * So setTimeout is needed, to make sure that
+   * speichern calls emitted from radios and checkboxes are delayed
+   */
+  var that = this
+  var wait = 0
+  if (this.type && (this.type === 'radio' || this.type === 'checkbox')) wait = 100
+
+  setTimeout(function () {
+    speichern(that)
+  }, wait)
 }
