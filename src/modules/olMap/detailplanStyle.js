97125e9439ad834bf2127ea5f86bb9b46ca031a2
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-24 09:39:10 +0100

    dont fill detailplaene

--- src/modules/olMap/detailplanStyle.js
+++ src/modules/olMap/detailplanStyle.js
@@ -4,9 +4,11 @@ var ol = require('ol')
 
 module.exports = function (feature, resolution) {
   return new ol.style.Style({
+    // mail vom 24.11.2016: lieber kein fill
+    /*
     fill: new ol.style.Fill({
       color: 'rgba(250, 58, 15, 0.1)'
-    }),
+    }),*/
     stroke: new ol.style.Stroke({
       color: '#fa3a0f',
       width: 1
