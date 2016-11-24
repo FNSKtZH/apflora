24aacd0937342e626d76219979ef5e07235572e8
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-24 10:13:08 +0100

    dont fill

--- src/modules/olMap/detailplanStyleSelected.js
+++ src/modules/olMap/detailplanStyleSelected.js
@@ -4,9 +4,10 @@ var ol = require('ol')
 
 module.exports = function (feature, resolution) {
   return new ol.style.Style({
+    /*
     fill: new ol.style.Fill({
       color: 'rgba(15, 85, 250, 0.1)'
-    }),
+    }),*/
     stroke: new ol.style.Stroke({
       color: '#0F55FA',
       width: 1
